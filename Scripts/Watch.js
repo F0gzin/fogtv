b616f02382bc187d97289c824fa3ccf5d3a086fd
F0g <150198436+F0gzin@users.noreply.github.com>
2026-09-03 02:07:33 -0300

    Add files via upload

---
+++ Scripts/Watch.js
@@ -0,0 +1,171 @@
+const params = Object.fromEntries(new URLSearchParams(window.location.search));
+const showId = params["s"];
+const episodeParam = params["e"];
+
+const playerVideoDiv = document.getElementById("PlayerVideoParent");
+const playerVideoTitle = document.getElementById("PlayerAreaTitle");
+
+let seasonId;
+let episodeId;
+let episodeData;
+
+const prevButton = document.getElementById("PrevEpisodeButton");
+const nextButton = document.getElementById("NextEpisodeButton");
+
+const cleanURL = window.location.origin + window.location.pathname;
+
+function loadFileSync(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false); 
+    xhr.send(null);
+
+    if (xhr.status === 200) {
+        return xhr.responseText;
+    } else {
+        throw new Error("Couldn't load file: "+String(url));
+    }
+}
+
+function LoadJsonFile(filePath){
+    const fileRawData = loadFileSync(filePath);
+    const fileAsObject = JSON.parse(fileRawData);
+    
+    return fileAsObject;
+}
+
+const episodeButtonTemplate = loadFileSync("../Data/EpisodeButton.html");
+
+const showsData = LoadJsonFile("../Data/ShowsData.json");
+const thisShowData = showsData[showId];
+
+if(thisShowData){
+    resolveEpisodeId(episodeParam);
+    loadShowEpisode(showId);
+    loadShowEpisodesPreview();
+}else{
+    window.alert(`Unknown show: "${showId}"`);
+    document.body.remove();
+}
+
+function resolveEpisodeId(episodeStr){
+    if(episodeStr === "$first" || episodeStr === "$1st" || episodeStr === undefined){
+        seasonId = 0;
+        episodeId = 0;
+        console.log("Episode:", episodeId, "; Season:", seasonId);
+        return;
+    }
+
+    const parts = episodeStr.split("_");
+    if(parts.length < 1){
+        return;
+    }else{
+        if(parts[1]){ // has two parts ex 1-2
+            seasonId = Number(parts[0])-1;
+            episodeId = Number(parts[1])-1;
+        }else{
+            seasonId = null;
+            episodeId = Number(parts[0])-1;
+        }
+    }
+
+    console.log("Episode:", episodeId, "; Season:", seasonId);
+}
+
+function loadShowEpisode(){
+    episodeData = thisShowData.seasons[seasonId].episodes[episodeId];
+    console.log("Episode data:", episodeData);
+    
+    let videoSourceUrl = episodeData.url_embed;
+
+    if(episodeData.host_type === "youtube"){
+        playerVideoDiv.innerHTML = `<iframe width="840" height="480" src="${videoSourceUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+    }else if(episodeData.host_type === "googledrive"){
+        playerVideoDiv.innerHTML = `<iframe id="PlayerVideo" src="${videoSourceUrl}" width="840" height="480"></iframe>`;
+    }else if(episodeData.host_type === "rawfile"){
+        playerVideoDiv.innerHTML = `<video id="PlayerVideo" width="840" height="480" controls><source src="${videoSourceUrl}" type="video/mp4"></video>`;
+    }else{
+        window.alert(`Unknown host: "${episodeData.host_type}"`);
+    }
+
+    //playerVideoTitle.innerHTML = `${thisShowData.display_name}: <i>${episodeData.name || "(episódio sem nome)"} - ${("Episódio " + String(episodeId+1))}</i>`;
+    if(episodeData.name){
+        playerVideoTitle.innerHTML = `${thisShowData.display_name}: <i>${episodeData.name || "(episódio sem nome)"} - ${("Episódio " + String(episodeId+1))}</i>`;
+    }else{
+        playerVideoTitle.innerHTML = `${thisShowData.display_name}: <i>${("Episódio " + String(episodeId+1))}</i>`;
+    }
+    document.title = `${seasonId+1}-${episodeId+1} - ${thisShowData.display_name}`;
+}
+
+function loadShowEpisodesPreview(){
+    const episodeListDiv = document.getElementById("EpisodesListA");
+    
+    for(let i=0; i<thisShowData.seasons.length; i++){
+        // for each season
+        const newLabel = document.createElement("h3");
+        newLabel.innerText = `Temporada ${String(i+1)}:`;
+        episodeListDiv.appendChild(newLabel);
+
+        for(let j=0; j<thisShowData.seasons[i].episodes.length; j++){
+            const newLabelA = document.createElement("div");
+            let Url = cleanURL + "?s="+showId+"&e="+`${i+1}_${j+1}`;
+            let ThumbUrl = "../Assets/ShowsThumb/"+showId+".png";
+            let processed = episodeButtonTemplate
+            .replaceAll("$HREF$",Url)
+            .replaceAll("$EPISODE_ID%",`${i+1} - ${j+1}`)
+            .replaceAll("$EPISODE_NAME%",thisShowData.seasons[i].episodes[j].name || "Ep") 
+            .replaceAll("$EPISODE_INFO%","Jan. 1st 1970")
+            .replaceAll("$THUMBNAIL_URL$",ThumbUrl);
+
+            if(i === seasonId && j === episodeId){
+                processed = processed.replaceAll("$STYLE$","background-color: #1f3ea7;");
+            }else{
+                processed = processed.replaceAll("$STYLE$","");
+            }
+            
+            newLabelA.innerHTML = processed;
+            episodeListDiv.appendChild(newLabelA);
+        }
+    }
+}
+
+function episodeToString(increase){
+    let EpNumber = episodeId;
+    let SeasonNumber = seasonId;
+
+    const seasonData = thisShowData.seasons[seasonId];
+
+    EpNumber += increase;
+
+    if(EpNumber < 0){
+        SeasonNumber--;
+    }else if(EpNumber >= seasonData.episodes.length){
+        EpNumber = 0;
+        SeasonNumber++;
+    }
+
+    if(SeasonNumber < 0){
+        window.alert("Nothing to go back to.");
+        return [false, "1_1"];
+    }
+
+    if(SeasonNumber >= thisShowData.seasons.length){
+        window.alert("It's over bro, nothing's more");
+        return [false, "1_1"];
+    }
+
+    return [true, `${SeasonNumber+1}_${EpNumber+1}`];
+}
+
+prevButton.onclick = function(){
+    const [success, s] = episodeToString(-1);
+    if(success){
+        window.location.assign(`${cleanURL}?s=${showId}&e=${s}`);
+    }
+}
+
+nextButton.onclick = function(){
+    const [success, s] = episodeToString(+1);
+    if(success){
+        window.location.assign(`${cleanURL}?s=${showId}&e=${s}`);
+    }
+}
